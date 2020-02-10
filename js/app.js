900501f40c4a1661302721b3a541bd5bc6c64c06
jacob kleiman <jacobakleiman@gmail.com>
2020-02-09 18:00:04 -0800

    Bubble falls with growing top margin with setTimeout

--- js/app.js
+++ js/app.js
@@ -5,6 +5,58 @@
 /* GLOBAL VARIABLES */
 const screens = []
 var score = 0
+let bubble_container = {
+    x: 0,
+    y: 0,
+    width: screen.width,
+    height: screen.height
+}
+
+/* CLASSES */
+class Bubble {
+    constructor(x=15,y=15) {
+        this.x = x
+        this.y = y
+        this.xV = 1
+        this. yV = 1
+        this.width = '50px'
+        this.height = '50px'
+        this.borderRadius = '50%'
+
+        //create div element and make appear
+        this.element = document.createElement('div')
+        this.appear()
+        // this.move()
+        const self = this
+        setInterval(this.move.bind(this), 100)
+       
+    }
+    move() {
+        this.x += 5
+        console.log(this)
+        this.y += 5
+        console.log(this.element)
+        this.element.style.marginTop = this.y+"px"
+        setTimeout(this.move, 2000);
+    }
+    getPos() {
+        return [this.x,this.y]
+    }
+    // collide() { //collision detection
+    //     if (this.x == bubble_container.width-5 || this.x == 0) {
+
+    //     }
+    // }
+    appear() {
+        this.element.classList.add('bubble')
+        this.element.addEventListener("click",pop)
+        //place bubble on screen
+        this.element.style.margin="15px"
+        //append child to bubble_container
+        document.querySelector('.bubble_container').appendChild(this.element)
+
+    }
+}
 
 /* FUNCTIONS */
 const loadScreens = () => {
@@ -49,7 +101,19 @@ const assignButtonListeners = () => {
     buttons[3].addEventListener("click",displaySplash)
 }
 const startGame = () => {
-    
+    //create a bubble
+    const bubble = new Bubble()
+    // bubble.move()
+    // bubble.appear()
+    // const bubble = document.createElement('div')
+    // bubble.classList.add('bubble')
+    // bubble.addEventListener("click",pop)
+    // //append child to bubble_container
+    // document.querySelector('.bubble_container').appendChild(bubble)
+}
+const pop = (event) => {
+    updateScore()
+    document.querySelector('.bubble_container').removeChild(event.target)
 }
 const updateScore = () => {
     score++
@@ -59,3 +123,6 @@ const updateScore = () => {
 /* GAME LOGIC*/
 loadScreens()
 assignButtonListeners()
+
+//for testing
+displayGame()
